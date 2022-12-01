cf91229f62744e78708fcb3ae5d95a1d620e94bd
Frederick Stallmeyer <fraizy@gmx.de>
2022-12-01 17:54:01 +0100

    give commands hints for import gpg keys to dev container

--- .devcontainer/add_gpg_commit_signatures.bash
+++ .devcontainer/add_gpg_commit_signatures.bash
@@ -4,6 +4,9 @@ set -e
 cat << EOF > gpg.key
 # Please add here your gpg public key long format export, for importing!!
 # Save and exit will import the gpg public key.
+# gpg --list-secret-keys --keyid-format=long
+# format -> e.g. rsa4096/<key-id>
+# gpg --armor --export <key-id>
 EOF
 nano gpg.key
 gpg --import gpg.key
